c384cf3b0da396f9b0c9572587ce66a74db62a26
donghyun <ehdgus094@hotmail.com>
2023-04-24 17:54:00 +0900

    firstCommit

---
+++ src/main/java/im/fitdiary/fitdiaryserver/FitdiaryServerApplication.java
@@ -0,0 +1,13 @@
+package im.fitdiary.fitdiaryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitdiaryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitdiaryServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/im/fitdiary/fitdiaryserver/FitdiaryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package im.fitdiary.fitdiaryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitdiaryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
