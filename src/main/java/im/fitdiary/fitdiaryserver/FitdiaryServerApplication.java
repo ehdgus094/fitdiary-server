55f75b6e0aa8f1257dabf6d5e335cb8c8efbab2e
donghyun <ehdgus094@hotmail.com>
2023-05-24 05:05:59 +0900

    logback 설정 추가

--- src/main/java/im/fitdiary/fitdiaryserver/FitdiaryServerApplication.java
+++ src/main/java/im/fitdiary/fitdiaryserver/FitdiaryServerApplication.java
@@ -10,6 +10,10 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 public class FitdiaryServerApplication {
 
 	public static void main(String[] args) {
+		// IPv6 -> IPv4 변환 (IntelliJ 빌드 시에는 적용 안됨)
+		System.setProperty("java.net.preferIPv4Stack", "true");
+		System.setProperty("java.net.preferIPv4Addresses", "true");
+
 		SpringApplication.run(FitdiaryServerApplication.class, args);
 	}
 
--- src/main/java/im/fitdiary/fitdiaryserver/config/WebConfig.java
+++ src/main/java/im/fitdiary/fitdiaryserver/config/WebConfig.java
@@ -2,6 +2,8 @@ package im.fitdiary.fitdiaryserver.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import im.fitdiary.fitdiaryserver.common.filter.MDCLoggingFilter;
+import im.fitdiary.fitdiaryserver.common.interceptor.RequestInfoLoggingInterceptor;
 import im.fitdiary.fitdiaryserver.exception.filter.HideErrorMessageFilter;
 import im.fitdiary.fitdiaryserver.security.argumentresolver.PrincipalArgumentResolver;
 import org.openapitools.jackson.nullable.JsonNullableModule;
@@ -11,6 +13,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.annotation.PostConstruct;
@@ -25,6 +28,13 @@ public class WebConfig implements WebMvcConfigurer {
         resolvers.add(new PrincipalArgumentResolver());
     }
 
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new RequestInfoLoggingInterceptor())
+                .order(1)
+                .addPathPatterns("/**");
+    }
+
     @PostConstruct
     public void setDefaultTimeZone() {
         TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
@@ -36,6 +46,15 @@ public class WebConfig implements WebMvcConfigurer {
         FilterRegistrationBean<HideErrorMessageFilter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new HideErrorMessageFilter(mapper));
         filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*");
+        return filterRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean<MDCLoggingFilter> mdcLoggingFilter() {
+        FilterRegistrationBean<MDCLoggingFilter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new MDCLoggingFilter());
+        filterRegistrationBean.setOrder(2);
         filterRegistrationBean.addUrlPatterns("/*");
         return filterRegistrationBean;
     }
