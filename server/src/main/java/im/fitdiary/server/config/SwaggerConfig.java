f41d48c55467cb9423fe0b0fa57fb52c98578302
donghyun <ehdgus094@hotmail.com>
2023-06-07 11:48:48 +0900

    swagger config 수정

--- server/src/main/java/im/fitdiary/server/config/SwaggerConfig.java
+++ server/src/main/java/im/fitdiary/server/config/SwaggerConfig.java
@@ -6,15 +6,34 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @ConfigurationLogging
 @Configuration
 public class SwaggerConfig {
 
+    @Profile("local")
     @Bean
-    public OpenAPI openAPI() {
+    public OpenAPI openAPILocal() {
+        return swaggerConfig();
+    }
+
+    @Profile("dev")
+    @Bean
+    public OpenAPI openAPIDev() {
+        List<Server> servers = new ArrayList<>();
+        servers.add(new Server().url("https://dev.fitdiary.im/api/v1"));
+        return swaggerConfig()
+                .servers(servers);
+    }
+
+    private OpenAPI swaggerConfig() {
         Info info = new Info()
                 .title("Fitdiary Server");
 
